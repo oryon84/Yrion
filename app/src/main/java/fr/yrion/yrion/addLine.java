c7de4409936ad4861933a9100a82573a95f33eb8
oryon84 <orion.isnard@gmail.com>
2015-11-07 00:42:35 +0100

    test3

---
+++ app/src/main/java/fr/yrion/yrion/addLine.java
@@ -0,0 +1,8 @@
+package fr.yrion.yrion;
+
+/**
+ * Created by oisn on 06/11/2015.
+ */
+public class addLine {
+
+}
--- app/src/main/java/fr/yrion/yrion/view_parcelle.java
+++ app/src/main/java/fr/yrion/yrion/view_parcelle.java
@@ -2,21 +2,27 @@ package fr.yrion.yrion;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class view_parcelle extends Activity {
     TextView titre;
     RecyclerView rv;
+    FloatingActionButton boutonadd;
     LinearLayoutManager llm;
     RecyclerView.Adapter adapter;
 
@@ -25,13 +31,29 @@ public class view_parcelle extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_parcelle);
         titre = (TextView)findViewById(R.id.titre);
+        boutonadd = (FloatingActionButton)findViewById(R.id.boutonadd);
+        boutonadd.setBackgroundTintList(getResources().getColorStateList(android.R.color.holo_red_light));
         rv = (RecyclerView)findViewById(R.id.rv);
         llm = new LinearLayoutManager(getApplicationContext());
         rv.setLayoutManager(llm);
         Intent intent = getIntent();
         String name = intent.getStringExtra("parcelle");
         titre.setText(name);
-
+        final ArrayList<String> mDataSet = new ArrayList<>();
+        boutonadd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mDataSet.add("toto");
+                RecyclerView.Adapter adapter = new RVAdapter(view_parcelle.this,mDataSet);
+                adapter.notifyDataSetChanged();
+                rv.setAdapter(adapter);
+            }
+        });
+        for(int i=0;i<10;i++){
+            mDataSet.add("Bonjour"+i);
+        }
+        RecyclerView.Adapter adapter = new RVAdapter(view_parcelle.this,mDataSet);
+        rv.setAdapter(adapter);
 
     }
 
